99f543186429f4755baf06c2ef5f6e24d8d0b5a0
Luc Roy <luc.roy@gmail.com>
2022-12-05 13:15:39 -0400

    fixed typo

--- README.md
+++ README.md
@@ -2,11 +2,6 @@
 This is a little script to help prepare the data from TCIA and move it to Estuary.
 
 ### Requirements
-Clone the TCIA Notebooks scripts to your project repository
-```
-git clone https://github.com/kirbyju/TCIA_Notebooks.git
-```
-
 Install the requirements by running:
 ```
 pip install -r requirements.txt
---
+++ tcia-to-estuary.py
@@ -0,0 +1,33 @@
+import TCIA_Notebooks.tcia_utils as tcia_utils
+import os
+import shutil
+
+from pestuary.collections import collection_create
+from pestuary.content import content_add
+
+def _rename(item):
+    return item[2] + "_" + item[4] + "_" + item[6] + "_" + item[7] + "_" + item[9] + "_" + item[10] + "_" + item[11]
+
+def _dataCleanup(collection):
+    shutil.rmtree(collection)
+    os.remove(collection + ".csv")
+
+def dataPrep(folderName, manifestList):
+    os.rename("tciaDownload", folderName)
+    for item in enumerate(manifestList):
+        split = item.split(",")
+        path = collectionName + "/" + split[1] + "/"
+        for file in os.listdir(path):
+            newName = _rename(split) + "_" + file
+            os.rename(os.path.join(path, file), os.path.join(path, newName))
+
+def getCollectionWithManifestList(collection):
+    series = tcia_utils.getSeries(collection)
+    tcia_utils.downloadSampleSeries(series, api_url = "", input_type = "", csv_filename=collection)
+    return tcia_utils.manifestToList(collection + ".csv")
+
+collectionName = "Soft-tissue-Sarcoma"
+list = getCollectionWithManifestList(collectionName)
+dataPrep(collectionName, list)
+responses = content_add(collectionName, create_collection=True)
+_dataCleanup(collectionName)
