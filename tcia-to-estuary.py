9d20a72ee4d294d88a94f5c16292e48c14be12d1
Luc Roy <luc.roy@gmail.com>
2022-12-05 23:06:51 -0400

    added gitignore and code cleanup

--- tcia-to-estuary.py
+++ tcia-to-estuary.py
@@ -2,41 +2,71 @@ import tcia_utils
 import os
 import shutil
 
-from pestuary.collections import collection_create
 from pestuary.content import content_add
 
-def _rename(item):
-    return item[2] + "_" + item[4] + "_" + item[6] + "_" + item[7] + "_" + item[9] + "_" + item[10] + "_" + item[11]
 
-def _dataCleanup(collection):
-    shutil.rmtree(collection)
-    os.remove(collection + ".csv")
+def rename(arr):
+    # Position : Field
+    # 1: Series UID
+    # 2: Collection
+    # 3: 3rd Party Analysis
+    # 4: Data Description URI
+    # 5: Subject ID
+    # 6: Study UID
+    # 7: Study Description
+    # 8: Study Date
+    # 9: Series Description
+    # 10: Manufacturer
+    # 11: Modality
+    # 12: SOP Class UID
+    # 13: Number of Images
+    # 14: File Size
+    # 15: File Location
+    # 16: Series Number
+    # 17: License Name
+    # 18: License URL
+    # 19: Annotation Size
+    return arr[2] + "_" + arr[4] + "_" + arr[5] + "_" + arr[8] + "_" + arr[9] + "_" + arr[11] + "_" + arr[1]
 
-def dataPrep(folderName, manifestList):
-    os.rename("tciaDownload", folderName)
-    for item in manifestList[1:]:
-        split = item.split(",")
-        path = collectionName + "/" + split[1] + "/"
+
+def cleanup(col):
+    shutil.rmtree(col)
+    os.remove(col + ".csv")
+
+
+def dataprep(folder, manifest):
+    os.rename("tciaDownload", folder)
+    for line in manifest[1:]:
+        split = line.split(",")
+        path = folder + "/" + split[1] + "/"
 
         for file in os.listdir(path):
-            newName = _rename(split) + "_" + file
-            os.rename(os.path.join(path, file), os.path.join(path, newName))
+            name = rename(split) + "_" + file
+            os.rename(os.path.join(path, file), os.path.join(path, name))
 
-def getCollectionWithManifestList(collection):
+
+def get_collection_with_manifest_list(collection):
     series = tcia_utils.getSeries(collection)
-    tcia_utils.downloadSampleSeries(series, api_url = "", input_type = "", csv_filename=collection)
+    tcia_utils.downloadSampleSeries(series, api_url="", input_type="", csv_filename=collection)
     return tcia_utils.manifestToList(collection + ".csv")
 
-collectionName = "Soft-tissue-Sarcoma"
-list = getCollectionWithManifestList(collectionName)
 
-print("### Preparing Collection Data")
-dataPrep(collectionName, list)
+def get_collection():
+    return tcia_utils.getCollections()
 
-print("### Adding Content to Estuary")
-responses = content_add(collectionName, create_collection=True)
 
-print("### Clean up")
-_dataCleanup(collectionName)
+for item in get_collection():
+    col_name = item['Collection']
+    print("### Fetching Collection " + col_name)
+    manifest_list = get_collection_with_manifest_list(col_name)
+
+    print("### Preparing Collection Data")
+    dataprep(col_name, manifest_list)
+
+    print("### Adding Content to Estuary")
+    responses = content_add(col_name, create_collection=True)
+
+    print("### Clean up")
+    cleanup(col_name)
 
 print("### END")
