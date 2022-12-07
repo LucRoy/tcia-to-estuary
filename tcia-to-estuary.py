aea5234211701e806427bf83bc7af15ce80b071c
Luc Roy <luc.roy@gmail.com>
2022-12-07 10:00:01 -0400

    fixed manifestToFile and added args for single colleciton upload

--- tcia-to-estuary.py
+++ tcia-to-estuary.py
@@ -1,3 +1,4 @@
+import argparse
 import tcia_utils
 import os
 import shutil
@@ -26,7 +27,7 @@ def rename(arr):
     # 17: License Name
     # 18: License URL
     # 19: Annotation Size
-    return arr[2] + "_" + arr[4] + "_" + arr[5] + "_" + arr[8] + "_" + arr[9] + "_" + arr[11] + "_" + arr[1]
+    return arr[2] + "_" + arr[5] + "_" + arr[8] + "_" + arr[9] + "_" + arr[11] + "_" + arr[1]
 
 
 def cleanup(col):
@@ -34,39 +35,58 @@ def cleanup(col):
     os.remove(col + ".csv")
 
 
-def dataprep(folder, manifest):
-    os.rename("tciaDownload", folder)
-    for line in manifest[1:]:
-        split = line.split(",")
-        path = folder + "/" + split[1] + "/"
+def dataprep(collection):
+    os.rename("tciaDownload", collection)
+    with open(collection + ".csv") as f:
+        next(f)
+        for line in f:
+            split = line.split(",")
+            path = collection + "/" + split[1] + "/"
 
-        for file in os.listdir(path):
-            name = rename(split) + "_" + file
-            os.rename(os.path.join(path, file), os.path.join(path, name))
+            for file in os.listdir(path):
+                name = rename(split) + "_" + file
+                os.rename(os.path.join(path, file), os.path.join(path, name))
 
 
 def get_collection_with_manifest_list(collection):
     series = tcia_utils.getSeries(collection)
     tcia_utils.downloadSampleSeries(series, api_url="", input_type="", csv_filename=collection)
-    return tcia_utils.manifestToList(collection + ".csv")
 
 
 def get_collection():
     return tcia_utils.getCollections()
 
 
-for item in get_collection():
-    col_name = item['Collection']
-    print("### Fetching Collection " + col_name)
-    manifest_list = get_collection_with_manifest_list(col_name)
+def upload_all_collections():
+    for item in get_collection():
+        collection = item['Collection']
+        upload_collection(collection)
+
+
+def upload_collection(collection):
+    print("### Fetching Collection " + collection)
+    get_collection_with_manifest_list(collection)
 
     print("### Preparing Collection Data")
-    dataprep(col_name, manifest_list)
+    dataprep(collection)
 
     print("### Adding Content to Estuary")
-    responses = content_add(col_name, create_collection=True)
+    # content_add(collection, create_collection=True)
 
     print("### Clean up")
-    cleanup(col_name)
+    cleanup(collection)
 
-print("### END")
+
+def main(options):
+    if options.collection is not None:
+        upload_collection(options.collection)
+    else:
+        upload_all_collections()
+    print("### END")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="TCIA Data preparation for uploading to Estuary")
+    parser.add_argument("--collection", dest='collection', help='Name of the collection to be uploaded.')
+    args = parser.parse_args()
+    main(args)
